b6aafb14f7514b655237d4b09db7d03a0638bea7
Raphael Randschau <nicolai86@me.com>
2014-02-04 19:31:12 +0100

    properly test the rake module run_command case

--- test/rake_test.py
+++ test/rake_test.py
@@ -35,6 +35,10 @@ class TestBase(unittest.TestCase):
     assert rake.diff('test/fixtures/current_db', 'test/fixtures/next_db') == False
     assert rake.diff('test/fixtures/changed_db', 'test/fixtures/next_db') == True
 
+    # missing_db folder does not exist
+    assert rake.diff('test/fixtures/missing_db', 'test/fixtures/next_db') == True
+    assert rake.diff('test/fixtures/missing_db', 'test/fixtures/missing_db') == True
+
 class TestRake(unittest.TestCase):
   def test_get_rake_path(self):
     module = FakeAnsibleModule()
@@ -44,6 +48,34 @@ class TestRake(unittest.TestCase):
     rake.get_rake_path()
 
     module.get_bin_path.assert_called_with('rake', True, [])
+
+  def test_run_command_with_unchanged_diff_paths(self):
+    module = FakeAnsibleModule()
+    module.get_bin_path = Mock()
+    module.params['path'] = '.'
+    module.params['diff_paths'] = [{
+      'current': 'test/fixtures/current_db',
+      'next': 'text/fixtures/next_db'
+    }]
+    module.run_command = Mock()
+
+    rake = RakeModule(module)
+    rake.run_command('db:migrate')
+    module.run_command.assert_not_called()
+
+  def test_run_command_with_hanged_diff_paths(self):
+    module = FakeAnsibleModule()
+    module.get_bin_path = Mock(return_value='/bin/rake')
+    module.params['path'] = '.'
+    module.params['diff_paths'] = [{
+      'current': 'test/fixtures/current_db',
+      'next': 'text/fixtures/changed_db'
+    }]
+    module.run_command = Mock()
+
+    rake = RakeModule(module)
+    rake.run_command('db:migrate')
+    module.run_command.assert_called_with(['/bin/rake', 'db:migrate'], check_rc=True)
 
 
 if __name__ == '__main__':
